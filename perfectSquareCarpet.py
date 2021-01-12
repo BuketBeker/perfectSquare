9f9ddebd9b57ed8a8194799c16540592f34507f9
Buket Beker <66417027+BuketBeker@users.noreply.github.com>
2021-01-12 23:17:13 +0300

    Update perfectSquareCarpet.py

--- perfectSquareCarpet.py
+++ perfectSquareCarpet.py
@@ -9,10 +9,9 @@ def calculate(field): # We find the largest possible perfect squares and print t
 
 while(True):
     try:
-        field = int(
-            input("Please enter the square meter of the field:")) # We take the square meter of the field from the user
-        if (field < 0): # The field cannot be negative
-            print("The field cannot be negative!!")
+        field = int(input("Please enter the square meter of the field:")) # We take the square meter of the field from the user
+        if (field < 0 or field == 0): # The field cannot be negative and zero
+            print("The field cannot be negative and zero!!")
             continue
         print("Output: ")
         calculate(field) # We assign the value we get from the user to the calculate function
